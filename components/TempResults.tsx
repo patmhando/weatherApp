0116977b8e8dae7d48c6ede101f8fe985a520f39
Patrick Mhando <patrickmhando@gmail.com>
2023-08-25 10:38:43 +0300

    add weather results comp

---
+++ components/TempResults.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+const TempResults: FC = () => {
+  return (
+    <tr className="text-sm">
+      <td>Day</td>
+      <td>Temperature</td>
+      <td>Weather Description</td>
+      <td>Wind Speed</td>
+      <td>Icon</td>
+    </tr>
+  );
+};
+export default TempResults;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,44 @@
+import { NextPage } from 'next';
 import { Inter } from 'next/font/google';
+
+// components
+import TempResults from '@/components/TempResults';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
-    <main className={`${inter.className}`}>
-      <div>
-        <label htmlFor="search">
-          <input type="text" />
+    <main
+      className={`${inter.className} my-8 flex flex-col justify-center items-center gap-4`}
+    >
+      <h1 className="text-center text-2xl uppercase">Weather Forecast App</h1>
+      <div className="flex gap-1 mx-8">
+        <label htmlFor="search" className="py-1 border px-2">
+          <input
+            type="text"
+            placeholder="Enter City"
+            name="search"
+            className="outline-none"
+          />
         </label>
+        <button className="border px-4 py-1 rounded">Search</button>
       </div>
+      <h3 className="text-xl">Weather Results</h3>
+      <table className="">
+        <thead className="text-sm">
+          <tr>
+            <th>Day</th>
+            <th>Temperature</th>
+            <th>Weather Description</th>
+            <th>Wind Speed</th>
+            <th>Icon</th>
+          </tr>
+        </thead>
+        <tbody>
+          <TempResults />
+        </tbody>
+      </table>
     </main>
   );
-}
+};
+export default Home;
