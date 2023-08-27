805ba3d85cd95b71ab8ebbdb41013d8300ccc030
Patrick Mhando <patrickmhando@gmail.com>
2023-08-27 10:28:11 +0300

    card colors

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,5 @@
 const Card = (props: any) => {
-  const classes = 'px-6 py-2 bg-gray-300 rounded' + props.className;
+  const classes = 'px-6 py-2 bg-gray-500 rounded' + props.className;
   return <div className={classes}>{props.children}</div>;
 };
 export default Card;
