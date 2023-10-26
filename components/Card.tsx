d6099db075346be90800b5320b8295208e297fd6
Patrick Mhando <patrickmhando@gmail.com>
2023-10-26 13:02:50 +0300

    change to app router

--- components/Card.tsx
+++
@@ -1,5 +0,0 @@
-const Card = (props: any) => {
-  const classes = 'px-6 py-2 bg-gray-500 rounded' + props.className;
-  return <div className={classes}>{props.children}</div>;
-};
-export default Card;
--- utils/index.ts
+++
@@ -1,6 +0,0 @@
-export const tempConvert = (tempCalc: number, temp: string): number => {
-  if (temp === 'C') {
-    return ((tempCalc - 32) * 5) / 9;
-  }
-  return tempCalc;
-};
